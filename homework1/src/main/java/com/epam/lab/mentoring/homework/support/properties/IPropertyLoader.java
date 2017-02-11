475482dc0ff7b7841da81efe9425fa23b4370f7f
Artsiom Tsaryonau <artsiom_tsaryonau@epam.com>
2017-02-11 20:45:22 +0300

    Adjusted property loading, added readme.txt

--- homework1/src/main/java/com/epam/lab/mentoring/homework/support/properties/IPropertyLoader.java
+++ homework1/src/main/java/com/epam/lab/mentoring/homework/support/properties/IPropertyLoader.java
@@ -1,5 +1,7 @@
 package com.epam.lab.mentoring.homework.support.properties;
 
+import java.util.Properties;
+
 public interface IPropertyLoader {
-    LoadStatus load();
+    Properties load();
 }
--- homework1/src/main/java/com/epam/lab/mentoring/homework/support/properties/SystemPropertyLoader.java
+++ homework1/src/main/java/com/epam/lab/mentoring/homework/support/properties/SystemPropertyLoader.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
+import static com.epam.lab.mentoring.homework.support.Constants.DEFAULT_FILE_NAME;
 import static com.epam.lab.mentoring.homework.support.Constants.TASK_FILE_KEY;
 
 public class SystemPropertyLoader implements IPropertyLoader {
@@ -13,16 +14,12 @@ public class SystemPropertyLoader implements IPropertyLoader {
     private static final Logger LOGGER = LoggerFactory.getLogger(SystemPropertyLoader.class);
 
     @Override
-    public LoadStatus load() {
+    public Properties load() {
         LOGGER.info("Attempt to load system properties.");
         Properties properties = new Properties();
 
         String repositoryFile = System.getProperty(TASK_FILE_KEY);
-        if (StringUtils.isBlank(repositoryFile)) {
-            return new LoadStatus(false);
-        }
-
-        properties.setProperty(TASK_FILE_KEY, repositoryFile);
-        return new LoadStatus(true, properties);
+        properties.setProperty(TASK_FILE_KEY, StringUtils.defaultIfBlank(repositoryFile, DEFAULT_FILE_NAME));
+        return properties;
     }
 }
