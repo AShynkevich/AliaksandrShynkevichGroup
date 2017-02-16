042d601635c6ffef4cc5d6e03dc549aa1711d7c3
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2017-02-16 14:17:38 +0300

    Added homework2 parent folder for tasks

--- homework2/homework2_5/src/com/epam/lab/mentoring/Main.java
+++ homework2/homework2_5/src/com/epam/lab/mentoring/Main.java
@@ -3,7 +3,8 @@ package com.epam.lab.mentoring;
 public class Main {
 
     public static void main(String[] args) {
-
+        // read file line by line and take first 3 characters
+        // add them to array list
     }
 
 }
