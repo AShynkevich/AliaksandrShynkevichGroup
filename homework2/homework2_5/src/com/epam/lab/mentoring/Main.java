2c19b136b95330ddb7d1f9d8bde4c51c60497597
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2017-02-16 15:50:48 +0300

    Basic task implementation with leak

--- homework2/homework2_5/src/com/epam/lab/mentoring/Main.java
+++ homework2/homework2_5/src/com/epam/lab/mentoring/Main.java
@@ -1,10 +1,34 @@
 package com.epam.lab.mentoring;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // read file line by line and take first 3 characters
-        // add them to array list
+    private static final String FILENAME = "Task #5 - Data.txt";
+
+    public static void main(String[] args) throws InterruptedException {
+        List<String> list = new ArrayList<String>();
+        try {
+            FileInputStream fis = new FileInputStream(FILENAME);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            try {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    list.add(line.substring(0, 3));
+                }
+            } finally {
+                br.close();
+            }
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+
+        Thread.sleep(20000);
     }
 
 }
