6ebc4425dd7a87792d7a0dfcb3554efdc272e018
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2017-03-16 10:08:21 +0300

    Updated service arguments names

--- homework4/soap-server/src/main/java/com/epam/lab/mentoring/webservice/soap/IFileSharingWebService.java
+++ homework4/soap-server/src/main/java/com/epam/lab/mentoring/webservice/soap/IFileSharingWebService.java
@@ -1,15 +1,21 @@
 package com.epam.lab.mentoring.webservice.soap;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 import java.util.List;
 
 @WebService
 public interface IFileSharingWebService {
+    @WebMethod
     List<String> listFiles();
 
-    void createFile(String filename, byte[] fileBytes);
+    @WebMethod
+    void createFile(@WebParam(name = "fileName") String filename, @WebParam(name = "bytes") byte[] fileBytes);
 
-    void deleteFile(String filename);
+    @WebMethod
+    void deleteFile(@WebParam(name = "fileName") String filename);
 
-    byte[] readFile(String filename);
+    @WebMethod
+    byte[] readFile(@WebParam(name="fileName") String filename);
 }
