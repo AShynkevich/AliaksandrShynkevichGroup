6f82f4c7d5c62b8c843a6a66ec9f80594e90fd9e
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2017-05-16 15:44:41 +0300

    Homework 8 initial commit

---
+++ homework8/src/main/java/com/epam/lab/mentoring/BookViewApplication.java
@@ -0,0 +1,13 @@
+package com.epam.lab.mentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookViewApplication.class, args);
+    }
+}
+
