c56d3f487fdd28207d6d18633eb605929f314656
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2017-05-18 16:49:17 +0300

    Upload handling

--- homework8/src/main/java/com/epam/lab/mentoring/rest/BooksWebController.java
+++ homework8/src/main/java/com/epam/lab/mentoring/rest/BooksWebController.java
@@ -2,21 +2,20 @@ package com.epam.lab.mentoring.rest;
 
 import com.epam.lab.mentoring.repository.filesystem.IBooksFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 
-@RestController("/books-rest")
+@RestController
 public class BooksWebController {
 
     @Autowired
     private IBooksFileRepository fileRepository;
 
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file) {
+    public ModelAndView uploadFile(@RequestParam("file") MultipartFile file) {
         try {
             if (!file.isEmpty()) {
                 fileRepository.writeFile(file.getName(), file.getBytes());
@@ -25,7 +24,7 @@ public class BooksWebController {
             e.printStackTrace();
         }
 
-        return "redirect:/books-home";
+        return new ModelAndView("redirect:/books-home");
     }
 
 }
