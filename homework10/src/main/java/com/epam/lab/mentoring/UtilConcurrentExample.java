0a16284454f8a27c09f78d3e0b9ccf2ad3e5e56a
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2017-06-08 10:15:45 +0300

    Changed executor shutdown strategy

--- homework10/src/main/java/com/epam/lab/mentoring/UtilConcurrentExample.java
+++ homework10/src/main/java/com/epam/lab/mentoring/UtilConcurrentExample.java
@@ -2,6 +2,7 @@ package com.epam.lab.mentoring;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class UtilConcurrentExample extends AbstractExample {
@@ -13,7 +14,12 @@ public class UtilConcurrentExample extends AbstractExample {
         });
         executorService.shutdown();
 
-        while (!executorService.isTerminated()) {}
+        try {
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Calculation result => " + output);
     }
 }
